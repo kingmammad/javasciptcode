e40b5fcdff638e4a59d3e59026c9c61502397e04
mohammad <kli868590@gmail.com>
2023-06-07 13:19:17 +0430

    4th compelete

--- challenge--day-7/verb.js
+++ challenge--day-7/verb.js
@@ -254,4 +254,15 @@ function getRandomHexColor() {
 }
 let ad = getRandomHexColor();
 console.log(ad);
-//
+//4th
+function hexToRGB(hex) {
+  hex = hex.replace("#", "");
+
+  const r = parseInt(hex.slice(0, 2), 16);
+  const g = parseInt(hex.slice(2, 4), 16);
+  const b = parseInt(hex.slice(4, 6), 16);
+
+  console.log(`rgb(${r},${g},${b})`);
+}
+
+hexToRGB("#297F3A");
