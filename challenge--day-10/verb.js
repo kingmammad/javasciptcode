e887d1bb49dfd0c18cfb508ea11951ea229e3210
mohammad <mo0788252@gmail.com>
2023-07-09 23:28:36 -0700

    complete 10 th

--- challenge--day-10/verb.js
+++ challenge--day-10/verb.js
@@ -12,48 +12,75 @@ for (let a = 0; a <= 10; a++) {
 let setloop = new Set(lop);
 console.log(setloop);
 //3th
-setloop.delete(3)
-console.log(setloop)
+setloop.delete(3);
+console.log(setloop);
 //4th
-setloop.clear()
+setloop.clear();
 //5th
-const arr=['akbar','shoja','asghar','haidar','ali'];
-let newarr=new Set(arr);
-console.log(newarr)
-for (elem of newarr){
-  console.log(elem)
-};
+const arr = ["akbar", "shoja", "asghar", "haidar", "ali"];
+let newarr = new Set(arr);
+console.log(newarr);
+for (elem of newarr) {
+  console.log(elem);
+}
 //6th
 let countrie = [
-  ['Finland', 'Helsinki'],
-  ['Sweden', 'Stockholm'],
-  ['Norway', 'Oslo'],
-]
-console.log(countrie)
-let map=new Map(countrie);
-console.log(map)
-for(elem of countrie){
- for (let a=0; a<=elem.length-1;a++){
-  map.set(elem[a], elem[a].length)
- }
+  ["Finland", "Helsinki"],
+  ["Sweden", "Stockholm"],
+  ["Norway", "Oslo"],
+];
+console.log(countrie);
+let map = new Map(countrie);
+console.log(map);
+for (elem of countrie) {
+  for (let a = 0; a <= elem.length - 1; a++) {
+    map.set(elem[a], elem[a].length);
+  }
 }
 console.log(map);
 //===================================level 2
 //1TH
-let c = [...a, ...b]
-let C = new Set(c)
-console.log(C)
+let c = [...a, ...b];
+let C = new Set(c);
+console.log(C);
 //2th
-let A=new Set(a);
-let B=new Set(b)
-let des=a.filter(elem=> B.has(elem))
-let dd=new Set(des)
-console.log(dd)
+let A = new Set(a);
+let B = new Set(b);
+let des = a.filter((elem) => B.has(elem));
+let dd = new Set(des);
+console.log(dd);
 //3th
-let count=countries.map(ele=>ele.languages).flat()
+let count = countries.map((ele) => ele.languages).flat();
+let ma = new Set(count);
+console.log(ma.size);
+//============================level 3th
+//1th
 
+function mostSpokenLanguages(countries, limit) {
+  const languageCounts = {};
 
-let ma=new Set(count)
-console.log(ma.size)
-// let sum=ma.reduce((ele,elem)=> ele+elem.length,0)
-// console.log(sum)
+  for (const country of countries) {
+    const languages = country.languages;
+
+    for (const language of languages) {
+      if (languageCounts.hasOwnProperty(language)) {
+        languageCounts[language]++;
+      } else {
+        languageCounts[language] = 1;
+      }
+    }
+  }
+
+  const sortedLanguages = Object.entries(languageCounts).sort(
+    (a, b) => b[1] - a[1]
+  );
+
+  const topLanguages = sortedLanguages.slice(0, limit);
+  const formattedLanguages = topLanguages.map(([language, count]) => ({
+    [language]: count,
+  }));
+
+  return formattedLanguages;
+}
+console.log(mostSpokenLanguages(countries, 14));
+dsfsdfsd
